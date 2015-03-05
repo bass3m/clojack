cd75113fc5edee88dda1afa593fb67f7bf4bdba1
Bassem Youssef <bassem@kumunetworks.com>
2015-03-05 12:17:01 -0800

    insultomatic: add ping

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [aleph "0.4.0-beta3"]
                  [com.taoensso/timbre "3.4.0"]
                  ;;[org.msync/properties-clj "0.3.1"]
--- src/insultomatic/server.clj
+++ src/insultomatic/server.clj
@@ -1,45 +1,56 @@
 (ns insultomatic.server
-  (:require
-   ;; [insultomatic.handler :as h]
-   [taoensso.timbre :as log]
-   [clojure.data.json :as json]
-   [environ.core :refer [env]]
-   [aleph.http :as http :only [get websocket-client]]
-   [manifold.stream :as s]
-   [byte-streams :as bs])
+  (:require [clojure.core.async :as async :refer [<! <!! go-loop thread]]
+            ;; [insultomatic.handler :as h]
+            [taoensso.timbre :as log]
+            [clojure.data.json :as json]
+            [environ.core :refer [env]]
+            [aleph.http :as http :only [get websocket-client]]
+            [manifold.stream :as s]
+            [byte-streams :as bs])
   (:gen-class))
 
 (def url "https://slack.com/api/rtm.start")
 
 (defn enable-logging
   []
-  (log/set-config! [:appenders :standard-out :rate-limit] [1 10000])
   (log/set-config! [:appenders :spit :enabled?] true)
   (log/set-config! [:shared-appender-config :spit-filename]
                    (or (env :logfile) "insultomatic.log")))
 
 (defn incoming-msg
   [slack-msg]
-  (log/info "Incoming msg:" slack-msg))
+  (log/info "Incoming msg:" (json/read-str slack-msg :key-fn keyword)))
 
 (defn outgoing-msg [])
 
+(defn pinger
+  [ws-stream]
+  (let [ping {:id 0
+              :type "ping"}]
+    (async/go-loop [id 0]
+      (<! (async/timeout 10000))
+      @(s/put! ws-stream
+               (json/write-str (assoc ping :id id)))
+      (recur (inc id)))))
+
 (defn event-loop
-  [ws-conn]
-  (loop [msg @(s/take! ws-conn)]
-    (incoming-msg msg)
-    (recur @(s/take! ws-conn))))
+  [ws-stream]
+  (pinger ws-stream)
+  (loop []
+    (when-let [msg @(s/take! ws-stream)]
+      (incoming-msg msg)
+      (recur))))
 
 (defn rtm-start
   "make a synchronous connection to slack and grab the ws url"
   []
-  (let [options {:query-params {:token (or (env :slack-token) token)}}
+  (let [options {:query-params {:token (env :slack-token)}}
         {:keys [status headers body error]} @(http/get
                                               "https://slack.com/api/rtm.start"
                                               options)]
     (if error
       (do
-        (println "Failed connecting to slack, exception is " error)
+        (log/error "Failed connecting to slack. Error:" error)
         nil)
       (as-> body _
         (bs/to-string _)
@@ -48,11 +59,8 @@
 
 (defn rtm-connect
   [ws-url]
-  (let [ws-conn @(http/websocket-client ws-url)]
-    (log/info "Got WS-conn:" ws-conn)
-    (s/consume incoming-msg ws-conn)
-    ;; (s/connect ws-conn ws-conn)
-    ws-conn))
+  (let [ws-stream @(http/websocket-client ws-url)]
+    ws-stream))
 
 (defn run []
   (enable-logging)
