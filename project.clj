89e30896cd5464e57a6f6de77806d2b7e79f177e
Bassem Youssef <bassem@kumunetworks.com>
2015-03-06 09:48:36 -0800

    clojack: adding project file after rename

--- project.clj
+++ project.clj
@@ -1,21 +1,18 @@
-(defproject insultomatic "0.1.0-SNAPSHOT"
+(defproject clojack "0.1.0-SNAPSHOT"
   :description "Slack Bot"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/bass3m/clojack"
+  :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [aleph "0.4.0-beta3"]
                  [com.taoensso/timbre "3.4.0"]
-                 ;;[org.msync/properties-clj "0.3.1"]
                  [environ "1.0.0"]
                  [org.clojure/data.json "0.2.5"]
-                 ;;[leiningen "2.5.1"]
                  [clj-time "0.9.0"]
                  [com.cemerick/piggieback "0.1.5"]]
   :plugins [[lein-environ "1.0.0"]
             [lein-ancient "0.6.1"]]
   :min-lein-version "2.5.0"
-  :uberjar-name "insultomatic.jar"
-  :main insultomatic.server
+  :uberjar-name "clojack.jar"
+  :main clojack.server
 )
