bf398c37dfabbe4a560e9f5a07286ed621ed325e
Bassem Youssef <bassem@kumunetworks.com>
2015-03-07 07:47:44 -0800

    clojack : little tiding up

--- src/clojack/handler.clj
+++ src/clojack/handler.clj
@@ -16,33 +16,31 @@
   [p]
   (eval (read-string (str "(clojack.plugins." p "/help)"))))
 
+(defn slack-response
+  [txt msg]
+  {:type "message"
+   :text txt
+   :channel (:channel msg)})
+
 (defn bot-help [msg ws-stream plugins]
   (log/info "HELP message:" msg ":with plugins:" plugins)
   (let [help-msg (->> plugins
                       (map plugin-help)
-                      (clojure.string/join "\n"))
-        rsp {:type "message"
-             :text help-msg
-             :channel (:channel msg)}]
-    @(s/put! ws-stream (json/write-str rsp))))
+                      (clojure.string/join "\n"))]
+    @(s/put! ws-stream (json/write-str (slack-response help-msg msg)))))
 
 (defn plugin-not-found
   [msg ws-stream]
   (let [flair [":confused:" ":sob:" ":cry:" ":scream:" ":poop:" ":question:"
                ":no_good:" ":scream_cat:" ":crying_cat_face:" ":goberserk:"
                ":hurtrealbad:" ":facepunch:"]
-        rsp {:type "message"
-             :text (str (rand-nth flair) " No comprendo " (:text msg))
-             :channel (:channel msg)}]
-    @(s/put! ws-stream (json/write-str rsp))))
+        txt (str (rand-nth flair) " No comprendo " (:text msg))]
+    @(s/put! ws-stream (json/write-str (slack-response txt msg)))))
 
 (defn plugin-run
   [plugin msg ws-stream]
-  (let [output (eval (read-string (str "(clojack.plugins." plugin "/run)")))
-        rsp {:type "message"
-             :text output
-             :channel (:channel msg)}]
-    @(s/put! ws-stream (json/write-str rsp))))
+  (let [output (eval (read-string (str "(clojack.plugins." plugin "/run)")))]
+    @(s/put! ws-stream (json/write-str (slack-response output msg)))))
 
 (defn bot-cmd
   [cmds msg ws-stream plugins]
@@ -50,22 +48,20 @@
     (plugin-run plugin msg ws-stream)
     (plugin-not-found msg ws-stream)))
 
-(defn insult [msg ws-stream]
-  (log/info "Insult message fool:" msg))
-
 (defmulti handle-message
   (fn [msg ws-stream plugins] (:type msg)))
 
 (defmethod handle-message "message"
   [msg ws-stream plugins]
   (log/info "Incoming Slack message:" msg ":plugins:" plugins)
-  (condp re-find (:text msg)
-    #"^!help\s++|^!help$" :>> (comp
-                               (fn [msg] (bot-help msg ws-stream plugins))
-                               (constantly msg))
-    #"^!(\w+)\s++|^!(\w+)$" :>> (fn [bot-cmd-matches]
-                                  (bot-cmd bot-cmd-matches msg ws-stream plugins))
-    (str "Not found")))
+  (when (:text msg)
+    (condp re-find (:text msg)
+      #"^!help\s++|^!help$" :>> (comp
+                                 (fn [msg] (bot-help msg ws-stream plugins))
+                                 (constantly msg))
+      #"^!(\w+)\s++|^!(\w+)$" :>> (fn [bot-cmd-matches]
+                                    (bot-cmd bot-cmd-matches msg ws-stream plugins))
+      (str "Not found"))))
 
 (defmethod handle-message :default
   [msg _ _]
